2bc2ed248dd949f0dbd9efb1f09e09ebe8716820
Jack <jackjibb@gmail.com>
2024-10-24 14:51:34 +0100

    t

--- Part2/jacobi2.c
+++ Part2/jacobi2.c
@@ -75,13 +75,14 @@ int main(int argc, char *argv[]) {
               (startTime.tv_sec * 1000000 + startTime.tv_usec);
   fprintf(csv, "%ld,", totalTime);
   fclose(csv);
+
   // print results
-  // printf("iter = %d  difmax = %9.11lf", iter, difmax);
-  // for (i = 0; i <= m + 1; i++) {
-  //   printf("\n");
-  //   for (j = 0; j <= n + 1; j++) {
-  //     printf("%3.5lf ", t[i][j]);
-  //   }
-  // }
-  // printf("\n");
+  printf("iter = %d  difmax = %9.11lf", iter, difmax);
+  for (i = 0; i <= m + 1; i++) {
+    printf("\n");
+    for (j = 0; j <= n + 1; j++) {
+      printf("%3.5lf ", t[i][j]);
+    }
+  }
+  printf("\n");
 }
