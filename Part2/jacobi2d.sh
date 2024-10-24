43f74712407ad782a2cc4e5f349c0d33868a1bfa
Jack <jackjibb@gmail.com>
2024-10-24 16:08:43 +0100

    t

--- Part2/jacobi2d.sh
+++ Part2/jacobi2d.sh
@@ -13,22 +13,22 @@ gcc jacobi2.c -O -o jacobi1
 gcc jacobi2.c -O2 -o jacobi2
 gcc jacobi2.c -O3 -o jacobi3
 
-for ((i = 100; i < 500; i += 10)); do
+for ((i = 100; i < 200; i += 2)); do
   echo $i
   ./jacobi0 $i $i .0001
 done
 
-for ((i = 100; i < 500; i += 10)); do
+for ((i = 100; i < 200; i += 2)); do
   echo $i
   ./jacobi1 $i $i .0001
 done
 
-for ((i = 100; i < 500; i += 10)); do
+for ((i = 100; i < 200; i += 2)); do
   echo $i
   ./jacobi2 $i $i .0001
 done
 
-for ((i = 100; i < 500; i += 10)); do
+for ((i = 100; i < 200; i += 2)); do
   echo $i
   ./jacobi3 $i $i .0001
 done
